b1a5c8ce4f57213e68e0350f4a87d27fcb4aecc3
Emrah AYDIN <91917229+EmrahAyd@users.noreply.github.com>
2023-01-20 00:57:21 +0300

    nearly

--- teslimaco.php
+++ teslimaco.php
@@ -7,9 +7,62 @@
     <title>TESLİM EDİLEN ANALİZ</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar','corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      let teslim = 0;
+      let donen = 0;
+      let ilisi = "";
+      function drawChart(ek,i,ce,x) {
+
+// Create the data table.
+var data = google.visualization.arrayToDataTable(ek);
+
+// Set chart options
+var options = {
+          chart: {
+            title: ce+' ilçesi',
+            colors: ['#388E3C', '#db4437', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+
+          }
+        };
+  var nameofchart = "chart_div"+i;
+  var chart = new google.charts.Bar(document.getElementById(nameofchart));
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+
+
+
+
+  var data = google.visualization.arrayToDataTable([
+          ['Durum', 'Adet'],
+          ['Teslim',     teslim],
+          ['Dönen',     donen],
+
+        ]);
+
+        var options = {
+          title: ilisi,
+          is3D: true,
+          colors: ['#388E3C', '#db4437', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+          width:1500,
+          height:500,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+}
+
+
+
+
+
+
+    </script>
 
 <?php
 $today = date("Y-m-d");
+$prev_date = date('Y-m-d', strtotime($date .' -30 day'));
 
 
 
@@ -27,6 +80,7 @@ $today = date("Y-m-d");
 
     <div class="container" style="margin-top: 100px ;">
         <div class="row">
+          <h1 id="ils" style="text-align: center;"></h1>
         <form action="/teslimatsor" method="post">
             <div class="col-md-6 offset-md-3" style="text-align: center; ">
                 <div class="" style="text-align: center; ">
@@ -133,13 +187,27 @@ $today = date("Y-m-d");
                     </div>
                 </div>
                 <div class="col-md-6 offset-md-3" style="display: block !important; text-aling:center;" >
-                    <input style="margin-top: 10px;" type="date" name="tarb" value="<?php $_POST["tarb"] ?>"> ile <input style="margin-top: 10px" type="date" name="tarbi" value="<?php echo $today; ?>"> arası.
+                    <input style="margin-top: 10px;" type="date" id="tarb" value="<?=$prev_date;?>"> ile <input style="margin-top: 10px" type="date" id="tarbi" value="<?php echo $today; ?>"> arası.
                 </div>
                 <div class="col-md-4 offset-md-4">
-                    <button class="button" type="submit" style="text-aling:center; margin-top: 15px; width: 100%; " name="sor">SORGULA</button>
                 </div>
             </div>
         </form>
+
+
+        <div class="row">
+          <div class="col-sm-12"><div id="chart_div" style="width: 500px;"></div></div>
+          <div class="col-sm-6"><div id="chart_div1"></div></div>
+          <div class="col-sm-6"><div id="chart_div2"></div></div>
+          <div class="col-sm-6"><div id="chart_div3"></div></div>
+          <div class="col-sm-6"><div id="chart_div4"></div></div>
+          <div class="col-sm-6"><div id="chart_div5"></div></div>
+          <div class="col-sm-6"><div id="chart_div6"></div></div>
+          <div class="col-sm-6"><div id="chart_div7"></div></div>
+          <div class="col-sm-6"><div id="chart_div8"></div></div>
+          <div class="col-sm-6"><div id="chart_div9"></div></div>
+
+        </div>
 
 
         </div>
@@ -156,24 +224,39 @@ $today = date("Y-m-d");
 
       $("#checkout-il").change(function() {
         var id = $(this).find("option:selected").val(); //alert(id);
-        console.log(id);
+        ilisi = $(this).find("option:selected").html(); //alert(id);
+        var star = $('#tarb').val();
+        var btar = $('#tarbi').val();
+        $('#ils').html(ilisi);
+        console.log(id,btar+' 00:00:00',star+' 00:00:00');
 
         $.ajax({
           url: "tjson",
           method: "GET",
           data: {
-            id: id
+            id: id,
+            star: star+' 00:00:00',
+            btar: btar+' 00:00:00'
           },
           success: function(data) {
             dataa = JSON.parse(data);
+            teslim = 0;
+            donen = 0;
+
             for (var i = 0; i < data.length; i++) {
 
-            console.log("Ilce: " + dataa[i].ilce);
-            console.log("Kargo Firma: " + dataa[i].arasT);
-            console.log("Durum 3 Sayisi: " + dataa[i].arasD);
-            console.log("Durum 4 Sayisi: " + dataa[i].pttT);
-            console.log("Durum 4 Sayisi: " + dataa[i].pttD);
+              teslim += parseInt(dataa[i].istD) + parseInt(dataa[i].arasD) + parseInt(dataa[i].gelD) + parseInt(dataa[i].verD) + parseInt(dataa[i].pttT) + parseInt(dataa[i].pttD);
+              donen += parseInt(dataa[i].istT) + parseInt(dataa[i].arasT) + parseInt(dataa[i].gelT) + parseInt(dataa[i].verT) + parseInt(dataa[i].pttT);
+              let ek = [['Firma', 'Teslim', 'Dönen'],
+                ['Aras'+(parseInt(dataa[i].istT),parseInt(dataa[i].istD)+1),parseInt(dataa[i].arasT)+1,parseInt(dataa[i].arasD)+1],
+                ['PTT',parseInt(dataa[i].pttT),parseInt(dataa[i].pttD)],
+                ['Verar',parseInt(dataa[i].verT),parseInt(dataa[i].verD)],
+                ['GelAl',parseInt(dataa[i].gelT),parseInt(dataa[i].gelD)],
+                ['KargoIST',parseInt(dataa[i].istT),parseInt(dataa[i].istD)]
 
+              ];
+
+              drawChart(ek,i+1,dataa[i].ilce,"Bar");
 
 }
 
